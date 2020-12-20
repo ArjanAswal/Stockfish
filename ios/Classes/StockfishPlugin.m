9ccb0c50a9932ab59772f3cea7599b30febbe395
Dao Hoang Son <daohoangson@gmail.com>
2020-12-20 22:47:58 +0700

    Use pure Java/Objective-C for the plugin

--- ios/Classes/StockfishPlugin.m
+++ ios/Classes/StockfishPlugin.m
@@ -1,15 +1,20 @@
 #import "StockfishPlugin.h"
-#if __has_include(<stockfish/stockfish-Swift.h>)
-#import <stockfish/stockfish-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "stockfish-Swift.h"
-#endif
 
 @implementation StockfishPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftStockfishPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"stockfish"
+            binaryMessenger:[registrar messenger]];
+  StockfishPlugin* instance = [[StockfishPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
